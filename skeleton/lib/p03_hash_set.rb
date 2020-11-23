95cce1fa8add32165d78499f124b8b713ce9d41c
stephen <stephenhyim@gmail.com>
2020-11-23 17:29:21 -0500

    completed hash set

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -3,17 +3,35 @@ class HashSet
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
+    @num_buckets = num_buckets
   end
 
   def insert(key)
+    
+    num = key.hash
+    
+    
+    if !self.include?(num)
+      self[num].push(key) 
+      self.count += 1
+    end
+    if self.count > @num_buckets
+      resize!
+    end
   end
 
   def include?(key)
-    
+    num = key.hash
+    self[num].include?(key)
 
   end
 
   def remove(key)
+    num = key.hash
+    if self.include?(key)
+      self[num].pop(key) 
+      self.count -= 1
+    end
   end
 
   private
@@ -30,4 +48,60 @@ class HashSet
 
   def resize!
   end
+
+
 end
+
+
+# class ResizingIntSet
+#   attr_accessor :count, :store
+
+#   def initialize(num_buckets = 20)
+#     @store = Array.new(num_buckets) { Array.new }
+#     @num_buckets = num_buckets
+#     @count = 0
+#   end
+
+#   def insert(num)
+#     if !self.include?(num)
+#       self[num].push(num) 
+#       self.count += 1
+#     end
+#     if self.count > @num_buckets
+#       resize!
+#     end
+#   end
+
+#   def remove(num)
+#     if self.include?(num)
+#       self[num].pop(num) 
+#       self.count -= 1
+#     end
+#   end
+
+#   def include?(num)
+#     self[num].include?(num)
+#   end
+
+#   private
+
+#   # def count
+#   #   overall_count = 0
+#   # end
+
+#   def [](num)
+#     # optional but useful; return the bucket corresponding to `num`
+#     i = num % @num_buckets
+#     self.store[i]
+#   end
+
+#   def num_buckets
+#     self.store.length
+#   end
+
+#   def resize!
+#     old_nums = self.store.flatten
+#    initialize(num_buckets * 2)
+#    old_nums.each do |int|
+#       insert(int)
+#    end
